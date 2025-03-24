564f31fa94676fb46a1cf7b9d33d9838432e8c33
nikhil <nikhilnaidu980@gmail.com>
2025-03-24 15:08:49 -0400

    Added config to create inbound rule for gke nodes

--- main.tf
+++ main.tf
@@ -14,18 +14,33 @@ resource "google_compute_network" "vpc" {
 
 #Creating pulic subnet
 resource "google_compute_subnetwork" "public_subnet" {
-    name = "public-subnet"
-    region = "us-west1"
-    network = google_compute_network.vpc.id 
-    ip_cidr_range = "10.0.1.0/24"
+    name                    = "public-subnet"
+    region                  = "us-west1"
+    network                 = google_compute_network.vpc.id 
+    ip_cidr_range           = "10.0.1.0/24"
     private_ip_google_access = true 
 }
 
 #Create private subnet
 resource "google_compute_subnetwork" "private_subnet" {
-    name = "private-subnet"
-    region = "us-west1"
-    network = google_compute_network.vpc.id 
-    ip_cidr_range = "10.0.2.0/24"
-    private_ip_google_access = true 
+    name                        = "private-subnet"
+    region                      = "us-west1"
+    network                     = google_compute_network.vpc.id 
+    ip_cidr_range               = "10.0.2.0/24"
+    private_ip_google_access    = true 
 }
+
+#Create a firewall rule to allow external access to GKe nodes
+resource "google_compute_firewall" "gke_allow_inbound" {
+    name        = "gke-allow-inbound"
+    network     = google_compute_network.vpc.id
+
+    allow {
+        protocol    = "tcp"
+        ports       = ["0-65535"]   
+    }
+
+    source_ranges   = ["0.0.0.0/0"]
+    traget_tags     = ["gke-node"]
+}
+
